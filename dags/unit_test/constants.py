4e85e9f81a1e000f486a3858ac818f2a396830ed
vishalgaikwad545 <116640713+vishalgaikwad545@users.noreply.github.com>
2023-09-26 21:54:53 +0530

    Update constants.py

--- dags/unit_test/constants.py
+++ dags/unit_test/constants.py
@@ -1,8 +1,8 @@
 # You can create more variables according to your project. The following are the basic variables that have been provided to you
-DB_PATH = "/home/Assignment/01_data_pipeline/scripts/unit_test/"
+DB_PATH = "/home/airflow/dags/unit_test/"
 DB_FILE_NAME = "utils_output.db"
 UNIT_TEST_DB_FILE_NAME = "unit_test_cases.db"
-DATA_DIRECTORY = "/home/Assignment/01_data_pipeline/scripts/unit_test"
+DATA_DIRECTORY = "/home/airflow/dags/unit_test/"
 INDEX_COLUMNS_TRAINING = ['created_date', 'city_tier', 'first_platform_c',
                 'first_utm_medium_c', 'first_utm_source_c', 'total_leads_droppped',
                 'referred_lead', 'app_complete_flag']
@@ -13,4 +13,4 @@ INDEX_COLUMNS_INFERENCE = ['created_date', 'city_tier', 'first_platform_c',
 
 NOT_FEATURES = ['created_date', 'assistance_interaction', 'career_interaction',
                 'payment_interaction', 'social_interaction', 'syllabus_interaction']
-INTERACTION_MAPPING = '/home/Assignment/01_data_pipeline/scripts/unit_test/interaction_mapping.csv'
+INTERACTION_MAPPING = '/home/airflow/dags/unit_test/interaction_mapping.csv'
